5cc854d2f731d173824c390c69fed20b2ab721cc
userjmmm <a@as-MacBook-Pro.local>
2024-07-31 17:21:13 +0900

    refactor: CategoryResponseData 타입을 Pageable에 맞추어 수정

--- src/api/hooks/useGetCategories.ts
+++ src/api/hooks/useGetCategories.ts
@@ -4,7 +4,34 @@ import type { CategoryData } from '@/types';
 
 import { BASE_URL, fetchInstance } from '../instance';
 
-export type CategoryResponseData = CategoryData[];
+export type CategoryResponseData = {
+  totalElements: number;
+  totalPages: number;
+  first: boolean;
+  last: boolean;
+  size: number;
+  content: CategoryData[];
+  number: number;
+  sort: {
+    empty: boolean;
+    sorted: boolean;
+    unsorted: boolean;
+  };
+  numberOfElements: number;
+  pageable: {
+    pageNumber: number;
+    pageSize: number;
+    sort: {
+      empty: boolean;
+      sorted: boolean;
+      unsorted: boolean;
+    };
+    offset: number;
+    paged: boolean;
+    unpaged: boolean;
+  };
+  empty: boolean;
+};
 
 export const getCategoriesPath = () => `${BASE_URL}/api/categories`;
 const categoriesQueryKey = [getCategoriesPath()];
